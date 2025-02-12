06acc908b85c58f8ab26487bf96cdf4a111ff2e5
Anshul Makhija <anshul0930.becse24@chitkara.edu.in>
2025-02-12 18:40:13 +0530

    Solution to Check Multiples

---
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int x,y;
+    scanf("%d %d",&x,&y);
+    if(x%y==0){
+        printf("Yes");
+    }
+    else{
+        printf("No")
+    }
+    return 0;
+}
